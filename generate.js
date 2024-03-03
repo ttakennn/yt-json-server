108d08be5b4feb15bac41c82058785fe5881f60f
KEN <ttakennn@gmail.com>
2024-03-03 16:32:55 +0700

    JSON server - setup + Create APIs + Auth

---
+++ generate.js
@@ -0,0 +1,67 @@
+import fs from 'fs';
+import { faker } from '@faker-js/faker/locale/vi';
+
+const randomCompanyList = (numberOfCompanies) => {
+  if (numberOfCompanies <= 0) return [];
+
+  const companyList = Array.from({ length: numberOfCompanies }).map(() => ({
+    id: faker.string.uuid(),
+    name: faker.company.name(),
+    createdAt: Date.now(),
+    updatedAt: Date.now(),
+    thumbnaiUrl: faker.image.urlPicsumPhotos({ with: 200, height: 200 }),
+  }));
+
+  return companyList;
+};
+
+const randomPersonList = (companyList, numberOfPerson) => {
+  if (numberOfPerson <= 0) return [];
+
+  const personList = [];
+
+  for (const company of companyList) {
+    for (let i = 0; i < numberOfPerson; i++) {
+      const person = {
+        id: faker.string.uuid(),
+        companyId: company.id,
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        age: faker.date.birthdate({ min: 18, max: 65, mode: 'age' }),
+        jobTitle: faker.person.jobTitle(),
+        email: faker.internet.email(),
+        phone: faker.phone.number(),
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+        avatarUrl: faker.image.avatar(),
+      };
+
+      personList.push(person);
+    }
+  }
+
+  return personList;
+};
+
+// IIFE
+(() => {
+  const numberOfCompanies = 10;
+  const numberOfPerson = 20;
+
+  const companyList = randomCompanyList(numberOfCompanies);
+  const personList = randomPersonList(companyList, numberOfPerson);
+
+  const db = {
+    companies: companyList,
+    persons: personList,
+    users: [],
+  };
+
+  fs.writeFile('db.json', JSON.stringify(db), (err) => {
+    if (err) {
+      console.error('Error writing to db.json: ', err);
+    } else {
+      console.log('Generate data successfully!');
+    }
+  });
+})();
---
+++ index.js
@@ -0,0 +1,68 @@
+import queryString from 'query-string';
+import jsonServer from 'json-server';
+import auth from 'json-server-auth';
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+
+// Add custom routes before JSON Server router
+server.get('/echo', (req, res) => {
+  res.jsonp(req.query);
+});
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now();
+  }
+  // Continue to JSON Server router
+  next();
+});
+
+// In this example, returned resources will be wrapped in a body property
+router.render = (req, res) => {
+  // Handle pagination
+  const totalCountResponse = Number.parseInt(res.get('X-Total-Count'));
+  console.log('totalCountResponse: ', totalCountResponse);
+
+  if (req.method === 'GET' && totalCountResponse) {
+    const queryParams = queryString.parse(req._parsedUrl.query);
+    console.log('queryParams: ', queryParams);
+
+    const body = {
+      data: res.locals.data,
+      pagination: {
+        _page: Number.parseInt(queryParams._page) || 1,
+        _limit: Number.parseInt(queryParams._limit) || 10,
+        _totalRows: totalCountResponse,
+      },
+    };
+
+    return res.jsonp(body);
+  }
+
+  return res.jsonp(res.locals.data);
+};
+
+// bind the router db to server
+server.db = router.db;
+
+// apply the auth middleware
+server.use(auth);
+
+//@faker-js/faker
+// Use default router
+// -> api/posts
+server.use('/api', router);
+
+// Start server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log('JSON Server is running');
+});
